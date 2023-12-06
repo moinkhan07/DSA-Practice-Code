7d690d0814c0b426179304f76bff75fd980b796b
moinkhan07 <moin6k@gmail.com>
2023-12-06 14:05:08 +0530

    updated power of 2 solution

--- 8.Recursion/PowerOf2.java
+++ 8.Recursion/PowerOf2.java
@@ -7,7 +7,8 @@ public class PowerOf2 {
 
     public static int powerOf2Optimised(int x,int n) { // O(logn)
         if(n == 0)return 1;
-        int halfPowerSqr = powerOf2(x, n/2) * powerOf2(x, n/2);
+        int halfPower =  powerOf2(x, n/2);
+        int halfPowerSqr = halfPower * halfPower;
         if(n % 2 != 0){
             halfPowerSqr = x * halfPowerSqr;
         }
